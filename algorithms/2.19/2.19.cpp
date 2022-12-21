19c114a2999441a08d1046703b86170942cca6f6
Иван Рыжих <117189369+IvanRyzhikh@users.noreply.github.com>
2022-12-21 12:13:48 +0500

    Add files via upload

---
+++ algorithms/2.19/2.19.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int S = 0;
+
+    cin >> N;
+
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 3 == 0)
+        {
+            S -= i;
+        }
+        else
+        {
+            S += i;
+        }
+    }
+    cout << " " << S << endl;
+    return 0;
+}
