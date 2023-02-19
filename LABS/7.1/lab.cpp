83b3e3310b92eadc41f65be506bfb6b8cc9183ad
Иван Рыжих <117189369+IvanRyzhikh@users.noreply.github.com>
2023-02-19 20:50:58 +0500

    Update lab.cpp

--- LABS/7.1/lab.cpp
+++ LABS/7.1/lab.cpp
@@ -14,34 +14,29 @@ void timeConvert(int* end_Hours, int* end_Minutes, int Minutes) {
 }
 
 void test1() {
-    int Hours;
+    int Hours, Minutes, end_Minutes;
+   
     std::cout << "enter hours: ";
     std::cin >> Hours;
-
-    int Minutes;
-    std::cout << "enter minutes: ";
     std::cin >> Minutes;
 
-    int end_Minutes;
     timeConvert(&end_Minutes, Hours, Minutes);
 
     std::cout << Hours << " hours + " << Minutes << " minutes -> " << end_Minutes << " minutes " << std::endl;
 }
 
 void test2() {
-
-    int Minutes;
+    int Minutes, end_Hours, end_Minutes;
+   
     std::cout << "enter minutes: ";
     std::cin >> Minutes;
 
-    int end_Hours, end_Minutes;
     timeConvert(&end_Hours, &end_Minutes, Minutes);
 
     std::cout << Minutes << " minutes -> " << end_Hours << " hours " << end_Minutes << " minutes " << std::endl;
 }
 
 int main() {
-
     test1();
     test2();
 }
